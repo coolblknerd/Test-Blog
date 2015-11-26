2934a8436175e479250c46e978ebf842933c6f1c
Reggie Davis <reginald.davis88@gmail.com>
2015-11-26 01:28:46 -0600

    Draft 1 of Styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,15 @@ class PostsController < ApplicationController
 	def new
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def create
 		@post = Post.new(post_params)
 		@post.save
 
 		redirect_to @post
-	end
-
-	def show
-		@post = Post.find(params[:id])
 	end
 
 	private
